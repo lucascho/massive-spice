0e09df99c190c7d1284a88acb142218ad492c585
Kaz Higaki <khigaki@sugarcrm.com>
2015-01-21 10:26:44 -0800

    updated extraction script to use opportunities instead of leads

--- upstream/db_extract.php
+++ upstream/db_extract.php
@@ -8,8 +8,27 @@
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     } 
-    $fields = ['salutation', 'title', 'facebook', 'twitter', 'googleplus', 'department', 'account_id', 'tag', 'lead_source', 'preferred_language', 'campaign_id'];
-    $sql = "SELECT status, " . implode(', ', $fields) . " FROM leads";
+    $fields = array(
+        "date_entered" => "date",
+        "date_modified" => "date",
+        "description" => "numeric",
+        "opportunity_type" => "string",
+        "campaign_id" => "string",
+        "lead_source" => "string",
+        "amount" => "numeric",
+        "base_rate" => "numeric",
+        "amount_usdollar" => "numeric",
+        "currency_id" => "string",
+        "date_closed" => "date",
+        "next_step" => "string",
+        "probability" => "numeric",
+        "best_case" => "numeric",
+        "worst_case" => "numeric",
+        "commit_stage" => "string",
+        "total_revenue_line_items" => "numeric",
+        "closed_revenue_line_items" => "numeric",
+    );
+    $sql = "SELECT sales_stage, " . implode(', ', array_keys($fields)) . " FROM opportunities";
     $result = $conn->query($sql);
     $output = array();
     // go through each row from the db
@@ -21,11 +40,18 @@
             $output[$status]['count'] = 0;
         }
         // go through each field from the results and capture it in the appropriate place
-        foreach ($fields as $field) {
+        foreach ($fields as $field => $type) {
             if (!$output[$status][$field]) {
-                $output[$status][$field] = array();
+                $output[$status][$field] = array(
+                    "type" => $type,
+                    "values" => array(),
+                );
             }
-            $output[$status][$field][] = $row[$field];
+            $value = $row[$field];
+            if ($field == "description") {
+                $value = strlen($value);
+            }
+            $output[$status][$field]['values'][] = $value;
         }
         $output[$status]['count']++;
     }
